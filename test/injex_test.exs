74c2f7440461d5dabb388ab4a43fff9fd5e98033
watanabe kenichro <kenichirow@watanabenoMacBook-Pro.local>
2021-03-07 14:42:48 +0900

    header

--- test/injex_test.exs
+++ test/injex_test.exs
@@ -5,6 +5,7 @@ defmodule InjexTest do
   test "greets the world" do
     conn = Plug.Test.conn("POST", "das/auth/testapp/test/register")
     conn = Plug.Conn.put_req_header(conn, "x-nativebase-fault-inject", "das-auth-failed")
+    conn = Plug.Conn.put_req_header(conn, "content-type", "application/json")
     conn = Injex.Plug.call(conn, Injex.Plug.init([]))
     assert conn.status == 401
   end
