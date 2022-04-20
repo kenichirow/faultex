7b9d004dfe70d409bdb5900e83117bc198278612
kenichirow <allagesxxx@gmail.com>
2022-04-20 22:12:12 +0900

    wip

--- lib/faultex.ex
+++ lib/faultex.ex
@@ -1,5 +1,23 @@
 defmodule Faultex do
   @moduledoc """
+    # Faultex
+
+   ### Global Parameters
+   
+   - disable: if true, disable all injectors
+   - injectors: list of injectors 
+   
+   ### Fault Injector Configuration
+   
+   In some request match parameters, you can set `"*"`. 
+   which means matches all incoming parameters.
+   
+   - disable: optional. if true, disable this injectors. if omit this parameter, set default to `false`
+   - host: optioanl. matches request host. if omit this parameters, set default to `"*"` 
+   - path: optional. matches pattern for request path. You can use Plug.Router style path parameters like `:id` and wildcard pattern like `/*path` default is `*`
+   - methd: optional. metches request method. atom or string. default is `"*"`
+   - header: optional. matches request headers. default is `[]`
+   - percentage: optional. default is `100`.
   """
 
   defmacro __using__(opts) do
